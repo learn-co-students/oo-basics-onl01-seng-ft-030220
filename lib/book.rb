82659dc89a93e73033a21fa16606f9617a8d0b38
Charles Butler <cdbutle@outlook.com>
2020-03-16 00:49:29 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
+  def title
+    @title = title
+  end
+  def title
+    @title
+  end
+  
+  attr_reader :author
+  attr_writer :author
+  attr_reader :page_count
+  attr_writer :page_count
+  attr_reader :genre
+  attr_writer :genre
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
 
 
